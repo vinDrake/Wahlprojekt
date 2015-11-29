5371dffae004cf1df45a641875cf58fc1d867af5
Benjamin Arnold <vin.drake1310@gmail.com>
2015-11-29 17:26:19 +0100

    User-Test
    100 User für Tests generiert und Test für "leeren" User angefangen

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,6 +2,17 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
-  #   assert true
+  #   assert true, "True is true"
   # end
+
+  # is equal to
+
+  # def test_the_truth
+  # assert true, "True is true"
+  # end
+
+  test "should not save user without name" do
+    user = User.new
+    assert_not user.save, "Saved the user without a name"
+  end
 end
