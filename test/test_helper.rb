6937d1c535366ce9a889d7541e2abcd0342020a9
Benjamin Arnold <vin.drake1310@gmail.com>
2015-11-29 18:04:57 +0100

    Base Fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,7 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  # fixtures :all
-  fixtures :users
+  fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
