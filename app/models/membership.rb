4337a6cf0a101d41ebfd8fae2abe8cbc0de5eba8
Shlx <magicmarvin@hotmail.de>
2015-12-12 16:45:40 +0100

    Erste Membership-Validatoren

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,6 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
+  validates :user, :group presence: true
+  validates :user, :group numericality: { only_integer: true } # Muessen beides IDs sein
 end
