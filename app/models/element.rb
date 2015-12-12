add47e8e92e39239605b076c2fb9ce89887dc143
Shlx <magicmarvin@hotmail.de>
2015-12-12 17:10:16 +0100

    Erste Element-Validatoren

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,4 +1,6 @@
 class Element < ActiveRecord::Base
   belongs_to :challenge
   belongs_to :question
+  validates :challenge, :question, presence: true
+  validates :challenge, :question, numericality: { only_integer: true }
 end
