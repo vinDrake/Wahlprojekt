8fdd327d413df918cf5250f2e6ddb9271d6a35ee
Shlx <magicmarvin@hotmail.de>
2015-12-12 17:05:38 +0100

    Erste Selection-Validatoren

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -2,4 +2,6 @@ class Selection < ActiveRecord::Base
   belongs_to :tag
   belongs_to :feeder
   has_one :user, through: :feeder
+  validates :tag, :feeder, presence: true
+  validates :tag, :feeder, numericality: { only_integer: true }
 end
