496995c865b1f8784032bf0ce9a792a66ec59b99
Shlx <magicmarvin@hotmail.de>
2015-12-12 17:08:59 +0100

    Erste Tie-Validatoren

--- app/models/tie.rb
+++ app/models/tie.rb
@@ -1,4 +1,6 @@
 class Tie < ActiveRecord::Base
   belongs_to :question
   belongs_to :tag
+  validates :question, :tag, presence: true
+  validates :question, :tag, numericality: { only_integer: true }
 end
