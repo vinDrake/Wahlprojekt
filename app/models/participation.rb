5260001be7799db4e35b05216fb67a07f3107927
Shlx <magicmarvin@hotmail.de>
2015-12-12 17:03:23 +0100

    Erste Participation-Validatoren

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,8 @@
 class Participation < ActiveRecord::Base
   belongs_to :user
   belongs_to :challenge
+  validates :user, :challenge, :strikes, presence: true
+  validates :user, :challenge, :strikes, numericality: { only_integer: true }
+  validates :complete, inclusion: { in: [true, false] }
+  validates :succeeded, inclusion: { in: [true, false] }
 end
