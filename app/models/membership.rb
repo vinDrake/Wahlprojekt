b2a5ffbbc23f2db3fe8c95f19503550380a1baa9
Shlx <magicmarvin@hotmail.de>
2015-12-12 16:50:55 +0100

    Erste Message-Validatoren

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,6 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
-  validates :user, :group presence: true
-  validates :user, :group numericality: { only_integer: true } # Muessen beides IDs sein
+  validates :user, :group, presence: true
+  validates :user, :group, numericality: { only_integer: true } # Muessen beides IDs sein
 end
