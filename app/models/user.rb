50e1bbd906c7a4009a078d9c31200d5d3caeace4
Benjamin Arnold <vin.drake1310@gmail.com>
2015-12-13 15:50:46 +0100

    New User -> New Feeder

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   validates :name, format: { with: /\A[a-zA-Z0-9]+\z/,
     message: "only allows letters and numbers" }, length: { in: 4..24 }
 
+  after_create :add_feeder_to_user
 
   has_many :repleys
   has_many :questions, through: :repleys
@@ -16,4 +17,11 @@ class User < ActiveRecord::Base
                           foreign_key: "receiver_id"
   has_many :sended_messages, class_name: "Message",
                           foreign_key: "sender_id"
+
+
+  private
+    def add_feeder_to_user
+      self.feeder = Feeder.new(:user_id => self.id)
+      self.feeder.save
+    end
 end
