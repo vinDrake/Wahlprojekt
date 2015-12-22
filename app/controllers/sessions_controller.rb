3edb832e5025a19cc5532690877cccd88fefbf6a
Benjamin Arnold <vin.drake1310@gmail.com>
2015-12-22 22:36:47 +0100

    Debugging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
         # format.json { render :show, status: :created, location: @user }
         format.json { }
       else
+        puts params
         format.html { redirect_to 'login' }
         # format.json { render "users/show", status: :created, location: @user }
 
