544ea8906a62dfb303c370010cc8bc4e34668d67
Benjamin Arnold <vin.drake1310@gmail.com>
2015-12-22 16:22:19 +0100

    CORS preflight

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,14 +16,35 @@ class ApplicationController < ActionController::Base
 
   # Begin CORS-Code
 
-  protect_from_forgery unless: -> { request.format.json? }
-  before_filter :set_cross_domain_access
+  # protect_from_forgery unless: -> { request.format.json? }
+  # before_filter :set_cross_domain_access
+  #
+  # def set_cross_domain_access
+  #   headers['Access-Control-Allow-Origin'] = '*'
+  #   headers['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, OPTIONS'
+  #   # headers['Access-Control-Allow-Headers'] = '*'
+  # end
 
-  def set_cross_domain_access
+  before_filter :cors_preflight_check
+after_filter :cors_set_access_control_headers
+
+def cors_set_access_control_headers
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  headers['Access-Control-Max-Age'] = "1728000"
+end
+
+def cors_preflight_check
+  if request.method == 'OPTIONS'
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, OPTIONS'
-    # headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+    headers['Access-Control-Max-Age'] = '1728000'
+
+    render :text => '', :content_type => 'text/plain'
   end
+end
 
   # End CORS
 end
