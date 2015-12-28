e3aa940799b353bb484d3d692e61123ec8495df7
Benjamin Arnold <vin.drake1310@gmail.com>
2015-12-28 15:27:46 +0100

    Validate Points

--- app/models/repley.rb
+++ app/models/repley.rb
@@ -2,5 +2,5 @@ class Repley < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
   belongs_to :answer
-  validates :user, :question, :answer, presence: true
+  validates :user, :question, :answer, :points, presence: true
 end
