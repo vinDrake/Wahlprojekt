880e1cd93bb532b4e9403930106ea2006aed2bf9
Benjamin Arnold <vin.drake1310@gmail.com>
2015-12-28 17:26:22 +0100

    Fragen aus einer Challenge in den Feeder stecken

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -44,6 +44,12 @@ class ParticipationsController < ApplicationController
 
     respond_to do |format|
       if @participation.save
+        # Anfang Fragen in Feeder
+        @challenge.questions.each do |question|
+          feed = Feed.new(:feeder_id => @user.feeder.id, :question_id => question.id, :priority => 1)
+          feed.save
+        end
+        # Ende Fragen in Feeder
         format.html { redirect_to @participation, notice: 'Participation was successfully created.' }
         format.json { render :show, status: :created, location: @participation }
       else
