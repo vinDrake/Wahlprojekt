71a3010459d03ac94307a667fe7de59a08eea3ce
Benjamin Arnold <vin.drake1310@gmail.com>
2016-01-09 14:48:18 +0100

    feeder.add_selection_feed_to_feeder

--- app/models/feeder.rb
+++ app/models/feeder.rb
@@ -2,6 +2,8 @@ class Feeder < ActiveRecord::Base
   belongs_to :user
   has_many :feeds
   has_many :questions, through: :feeds
+  has_many :selections
+  has_many :tags, through: :selections
   validates :user, presence: true
 
   after_create :add_feed_to_feeder
@@ -12,9 +14,36 @@ class Feeder < ActiveRecord::Base
     return self.feeds.order(priority: :desc).first
   end
 
-  private
-    def add_feed_to_feeder
+  # OPTIMIZE Hübscher und sinnvoller sollte das schon sein
+  # TODO Dokumentation
+  def add_feed
+    self.add_selection_feed_to_feeder
+  end
 
+  # OPTIMIZE Das lässt sich bestimmt mit einem "einfachen" Aufruf viel eleganter lösen.
+  def add_selection_feed_to_feeder
+    # Go through questions in random order
+    Question.order("RANDOM()").each do |question|
+      # Go through tags of the question
+      question.tags.each do |question_tag|
+        # Compare tags with selections
+        self.tags.each do |selection_tag|
+          if question_tag == selection_tag
+            feed = Feed.new(:feeder_id => self.id, :question_id => question.id, :priority => 0)
+            feed.save
+            return
+          end
+        end
+      end
+    end
+    # When no matching questions is found, take a random one
+    question = Question.order("RANDOM()").first
+    feed = Feed.new(:feeder_id => self.id, :question_id => question.id, :priority => 0)
+    feed.save
+  end
+
+  private
+    def add_feed_to_feeder # OPTIMIZE Zwei fast identische Methoden sind nicht sehr hübsch
       question = Question.order("RANDOM()").first
       feed = Feed.new(:feeder_id => self.id, :question_id => question.id, :priority => 0)
 
