ccd60e568a753f5b964887976c7c63cb161fcad3
Benjamin Arnold <vin.drake1310@gmail.com>
2016-01-10 18:50:18 +0100

    ...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -81,7 +81,7 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       # params.require(:user).permit(:name, :email, :password)
-      params.permit(:id, :name, :email, :password)
+      params.permit(:id, :name, :email, :password).require(:user).permit(:name, :email, :password)
 
     end
 end
