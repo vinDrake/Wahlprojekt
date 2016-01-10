bffce0dd1ef9823f58bf34a38bca09dc1a34b069
Benjamin Arnold <vin.drake1310@gmail.com>
2016-01-10 18:19:45 +0100

    Dirty User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -82,7 +82,7 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       # params.require(:user).permit(:name, :email, :password)
-      params.permit(:name, :email, :password)
+      params # .permit(:name, :email, :password)
 
     end
 end
