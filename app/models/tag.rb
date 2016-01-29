cc809ad5b06bd92c2f7439d0b85e7ec64bed41cf
Benjamin Arnold <vin.drake1310@gmail.com>
2016-01-29 05:31:10 +0100

    Unique Tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,11 +4,11 @@ class Tag < ActiveRecord::Base
   has_many :selections, dependent: :destroy
   has_many :feeder, through: :selections
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   # TODO Dokumentieren
 
-  # TODO Validieren, ob es schon ein gleiches/ähnliches gibt
+  # TODO Validieren, ob es schon ein gleiches/ähnliches gibt - DONE
 
 
 end
