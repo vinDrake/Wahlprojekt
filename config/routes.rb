4510b24ff1fe56a81d43e58a1d7ba34defaa5d37
Benjamin Arnold <vin.drake1310@gmail.com>
2016-02-02 07:25:43 +0100

    Route für Startseite

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'users#index'
+  root 'repleys#new'
 
 
   # Example of regular route:
