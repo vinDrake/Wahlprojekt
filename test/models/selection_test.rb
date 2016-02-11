5c998c6aa4a4cebe21f4944439dea5f73cb106ee
Benjamin Arnold <vin.drake1310@gmail.com>
2016-02-11 13:59:03 +0100

    Selection tests

--- test/models/selection_test.rb
+++ test/models/selection_test.rb
@@ -4,4 +4,14 @@ class SelectionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def test_the_truth
+    assert true
+  end
+
+  def test_create_selection
+    selection = Selection.new( tag: Tag.all.first , feeder: Feeder.all.first )
+    assert selection.save
+  end
+
 end
