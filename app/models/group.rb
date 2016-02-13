6d2219669b419852e78c75a99691bce2205846bc
Benjamin Arnold <vin.drake1310@gmail.com>
2016-02-13 16:53:47 +0100

    Group Tests

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
   has_many :user, through: :memberships
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { in: 3..255 }
 end
